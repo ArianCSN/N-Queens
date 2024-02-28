a6a08f19a08ec1f873e1521ba05f54b9d601b900
ArianCSN <arianniazi2002@gmail.com>
2024-02-28 16:49:59 +0330

    v2.0

--- N-queens.py
+++ N-queens.py
@@ -1,4 +1,23 @@
 import random
+import pygame
+import time 
+import os 
+from pygame.locals import (
+    MOUSEBUTTONUP,
+    K_ESCAPE,
+    KEYDOWN,
+    QUIT,
+)
+ 
+pygame.init()
+win = pygame.display.set_mode((800,800))
+win.fill((255,255,255))
+clock = pygame.time.Clock()
+board = [['  ' for i in range(8)] for i in range(8)]
+current_dir = os.path.dirname(os.path.abspath(__file__))
+queen_path = os.path.join(current_dir, "chessqueen.png")
+
+Q = pygame.image.load(queen_path)
 
 def check_is_digit(input):
     if input.strip().isdigit():
@@ -6,30 +25,9 @@ def check_is_digit(input):
     else:
         return 0
 
-board = [
- [".", ".", ".", ".", ".", ".", ".", "."]
-,[".", ".", ".", ".", ".", ".", ".", "."]
-,[".", ".", ".", ".", ".", ".", ".", "."]
-,[".", ".", ".", ".", ".", ".", ".", "."]
-,[".", ".", ".", ".", ".", ".", ".", "."]
-,[".", ".", ".", ".", ".", ".", ".", "."]
-,[".", ".", ".", ".", ".", ".", ".", "."]
-,[".", ".", ".", ".", ".", ".", ".", "."]]
-
 list = [0, 1, 2, 3, 4, 5, 6, 7]
 
-number = input("Enter the row number for the first queen (1 - 8): ")
-if check_is_digit(number)  :
-    number = int(number)
-    number -=1
-    if 0<= number <=7 :
-        board[0][number] = 'Q' 
-    else :
-        print("Invalid input! The program will randomly select a position between 1 and 8.")
-        board[0][random.choice(list)] = 'Q' 
-else :
-    print("Invalid input! The program will randomly select a position between 1 and 8.")
-    board[0][random.choice(list)] = 'Q'
+board[0][random.choice(list)] = 'Q'
 
 
 def attack(i, j):
@@ -51,12 +49,23 @@ def N_queens(n):
                 board[i][j] = 'Q'
                 if N_queens(n-1)==True:
                     return True
-                board[i][j] = "."
+                board[i][j] = "  "
     return False
 
 N_queens(8)
 
+
+
 for i in range(8):
-    print()
     for j in range(8):
-        print (board[i][j] , end=" ")
+        if board[i][j] == "  ":
+            pygame.draw.rect(win,(255,255,255),[i*100,j*100,100,100])
+        if board[i][j] == "Q":
+            pygame.draw.rect(win,(0,0,0),[i*100,j*100,100,100])
+pygame.display.update()
+
+run = True
+while run :
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
