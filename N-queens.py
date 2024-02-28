f44500ca130d3187adb19d911605f40e5399a1e4
ArianCSN <arianniazi2002@gmail.com>
2024-02-28 16:38:00 +0330

    v1.2

--- N-queens.py
+++ N-queens.py
@@ -1,5 +1,11 @@
 import random
 
+def check_is_digit(input):
+    if input.strip().isdigit():
+        return 1
+    else:
+        return 0
+
 board = [
  [".", ".", ".", ".", ".", ".", ".", "."]
 ,[".", ".", ".", ".", ".", ".", ".", "."]
@@ -10,24 +16,38 @@ board = [
 ,[".", ".", ".", ".", ".", ".", ".", "."]
 ,[".", ".", ".", ".", ".", ".", ".", "."]]
 
+list = [0, 1, 2, 3, 4, 5, 6, 7]
 
-def check(i, j):
+number = input("Enter the row number for the first queen (1 - 8): ")
+if check_is_digit(number)  :
+    number = int(number)
+    number -=1
+    if 0<= number <=7 :
+        board[0][number] = 'Q' 
+    else :
+        print("Invalid input! The program will randomly select a position between 1 and 8.")
+        board[0][random.choice(list)] = 'Q' 
+else :
+    print("Invalid input! The program will randomly select a position between 1 and 8.")
+    board[0][random.choice(list)] = 'Q'
+
+
+def attack(i, j):
     for k in range(0,8):
         if board[i][k]=="Q" or board[k][j]=="Q":
-            return False
+            return True
     for k in range(0,8):
         for l in range(0,8):
             if (k+l==i+j) or (k-l==i-j):
                 if board[k][l]=="Q":
-                    return False
-    return True
-
+                    return True
+    return False
 def N_queens(n):
-    if n==0:
+    if n==1:
         return True
-    for i in range(0,8):
+    for i in range(1,8):
         for j in range(0,8):
-            if (check(i,j)) and (board[i][j]!="Q"):
+            if (not(attack(i,j))) and (board[i][j]!="Q"):
                 board[i][j] = 'Q'
                 if N_queens(n-1)==True:
                     return True
